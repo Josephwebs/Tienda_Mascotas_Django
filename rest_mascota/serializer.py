3d7b6f45cfdbc48d0285c6b367e15adb0933b1fe
JosephWebs <jose_luis967@hotmail.com>
2022-06-09 20:04:49 -0400

    prueba

--- rest_mascota/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import Mascota
-
-class MascotaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mascota
-        fields = ['idMascota', 'nombreMascota', 'edadAnioMascota','edadMesesMascota','sexoMascota','estEstirizadoMascota','razaMascota','fotoMascota']
-    
